8fe416a68172727b9d0f32719a11118e83d2be44
Анастасия Сахарова <elliot.wrt@gmail.com>
2016-09-10 11:49:02 +0300

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const webpack = require('webpack');
+const path = require('path');
+const loaders = require('./webpack.loaders');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const host = process.env.HOST || "127.0.0.1";
+const port = process.env.PORT || "8888";
+
+// local scss modules
+loaders.push({
+	test: /[\/\\]src[\/\\].*\.scss/,
+	loaders: [
+		'style?sourceMap',
+		'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+		'sass'
+	]
+});
+
+module.exports = {
+	entry: [
+		`webpack-dev-server/client?http://${host}:${port}`,
+		`webpack/hot/only-dev-server`,
+		`./src/index.jsx` // entry point
+	],
+	devtool: process.env.WEBPACK_DEVTOOL || 'cheap-module-source-map',
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		loaders
+	},
+	devServer: {
+		contentBase: "./public",
+		// do not print bundle build stats
+		noInfo: true,
+		// enable HMR
+		hot: true,
+		// embed the webpack-dev-server runtime into the bundle
+		inline: true,
+		// serve index.html in place of 404 responses to allow HTML5 history
+		historyApiFallback: true,
+		port: port,
+		host: host
+	},
+	plugins: [
+		new webpack.NoErrorsPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/template.html'
+		}),
+	]
+};
